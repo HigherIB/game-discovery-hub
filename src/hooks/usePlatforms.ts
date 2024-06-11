ed200bbdbb7ff1493a631d86946a658137df6a1d
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-11 08:55:36 +0000

    Fetching games with react query and retiring useData hook

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 interface Platform {
   id: number;
@@ -11,7 +10,10 @@ interface Platform {
 
 const usePlatforms = () => useQuery({ 
   queryKey: ['platforms'],
-  queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms').then(res => res.data),
+  queryFn: () => 
+    apiClient
+      .get<FetchResponse<Platform>>('/platforms/lists/parents')
+      .then(res => res.data),
   staleTime: 24 * 60 * 60 * 1000,
   initialData: { count: platforms.length, results: platforms }
  });
