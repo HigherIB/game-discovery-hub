c571354e78b97ca70cef5b45bac8237c2c196769
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-18 18:02:06 +0000

    Color mode toggle theme update

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,11 +1,21 @@
 import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+import '../theme';
 
 const ColorModeSwitch = () => {
   const {toggleColorMode, colorMode} = useColorMode();
 
+  const togleTheme =() =>{
+    const root = document.getElementsByTagName('html')[0];
+    root.classList.toggle('dark');
+    // @ts-ignore
+    if (!document.startViewTransition) toggleColorMode();
+    // @ts-ignore
+    document.startViewTransition(() => toggleColorMode ());
+  }
+
   return (
     <HStack>
-      <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+      <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={togleTheme} />
       <Text whiteSpace='nowrap'>{colorMode === 'dark' ? 'Light' : 'Dark'} Mode</Text>
     </HStack>
   )
